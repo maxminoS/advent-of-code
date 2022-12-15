947b1f805ac18036b2f102ef522b8990133f2416
maxminoS <maxminos.1@gmail.com>
2022-12-15 15:28:02 +0700

    solve day 5 part 1 puzzle

---
+++ 2022/src/aoc2022/day05.clj
@@ -0,0 +1,72 @@
+(ns aoc2022.day05
+  (:require
+   [clojure.string :as str]))
+
+(def demo-input "    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2")
+
+(def input (slurp "resources/input/day-05.txt"))
+
+(defn transpose-matrix [mat]
+  (map reverse (apply mapv vector mat)))
+
+(defn pad-crates [seq len]
+  (concat seq (repeat (- len (count seq)) \space)))
+
+(defn parse-crates [crates stacks]
+  (->> crates
+       str/split-lines
+       pop
+       (map #(map (fn [x] (second x)) (partition 3 4 %)) )
+       (map #(pad-crates % stacks))
+       transpose-matrix
+       (map #(filterv (fn [x] (not (= x \space))) %))))
+
+(defn parse-instructions [instructions]
+  (->> instructions
+       str/split-lines
+       (map #(take-nth 2 (rest (str/split % #" "))))
+       flatten
+       (map #(Integer/parseInt %))
+       (partition 3)))
+
+(defn arrange [crates [move from to]]
+  (let [from-stack (nth crates (dec from))
+        crates-to-move (reverse (take move (reverse (nth crates (dec from)))))
+        from-stack-moved (take (- (count from-stack) move) from-stack)                                  ; M
+        to-stack-moved (concat (nth crates (dec to)) (reverse crates-to-move))
+        moved (-> (vec crates)
+                   (assoc (dec from) from-stack-moved)
+                   (assoc (dec to) to-stack-moved))]
+    moved))
+
+(defn parse-input [input-data]
+  (let [parsed (str/split input-data #"\n\n")
+        stacks (-> (first parsed) str/split-lines last str/trim (str/split #"\s+") last Integer/parseInt)
+        crates (parse-crates (first parsed) stacks)
+        instructions (parse-instructions (last parsed))]
+    [crates instructions]))
+
+(parse-input demo-input)            ;; [[Z N] [M C D] [P]]
+
+(defn solve-puzzle-1 [input-data]
+  (let [parsed (parse-input input-data)
+        crates (first parsed)
+        instructions (last parsed)
+        final-pos (loop [curr-crate crates
+                         inst-id 0]
+                    (if (= (count instructions) inst-id)
+                      curr-crate
+                      (recur (arrange curr-crate (nth instructions inst-id))
+                             (inc inst-id))))
+        top-crates (map last final-pos)]
+    (str/join "" top-crates)))
+
+(solve-puzzle-1 input)
